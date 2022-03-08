e0752acdb7d1039f9e4dcb7a1e65560643c61eca
liuzhipeng <aliuzhipeng_v@didiglobal.com>
2022-03-08 16:09:50 +0800

    样式优化

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,6 +7,7 @@ import {
   RadiusBottomleftOutlined,
   RadiusBottomrightOutlined,
 } from '@ant-design/icons';
+import ICON,{pup} from './svg'
 import './index.scss'
 
 const Context = React.createContext({ name: 'Default' });
@@ -15,7 +16,7 @@ export default function Home({ history }) {
 
   const [showModal, setShowModal] = useState(false);
   const [api, contextHolder] = notification.useNotification();
-  const [play, setPlay] = useState(true);
+  const [, setStart] = useState(true);
 
   useEffect(() => {
     clickEffect(true)
@@ -47,9 +48,9 @@ export default function Home({ history }) {
   };
   const autoPlay = () => {
     const myMusic = document.getElementById('myMusic')
-    const playPromise = myMusic.play(); //chrom 5.0 以上 play() 需要返回promise
-    setPlay(play => {
-      if (play&&playPromise !== null) {
+    const playPromise = myMusic?.play(); //chrom 5.0 以上 start() 需要返回promise
+    setStart(start => {
+      if (start&&playPromise !== null) {
         playPromise.then(() => {
           myMusic.play()
       }).catch(e=> {
@@ -59,7 +60,7 @@ export default function Home({ history }) {
       } else {
         myMusic.pause();
       }
-      return !play
+      return !start
     })
 
 
@@ -76,7 +77,7 @@ export default function Home({ history }) {
       </p>
       <p className={"title"} onClick={toPath}  >Welcome to my blog</p>
       <div className="toggle">
-        <div className="toggle_body" onClick={showModalFun} style={showModal ? { transform: 'scale(.8,.8)' } : {}}>
+        <div className={showModal ? "toggle_body showModalAndToggleBody" :'toggle_body '} onClick={showModalFun} >
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(i => (
               <div key={i}></div>
@@ -96,23 +97,24 @@ export default function Home({ history }) {
       >
         <div className={'modalCenter'}>
           {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(i => (
-              <div key={i}>
-                <svg version="1.1" x="0px" y="0px" width="72px" height="72px" viewBox="0 0 72 72">
-                  <path d="M67.572,37.08L38.686,8.168c-1.476-1.534-3.892-1.534-5.368,0L4.428,37.08c-1.48,1.529-0.929,2.786,1.224,2.786h6.023V62.07	c0,1.603,0.068,2.912,2.966,2.912h14.022V42.706h14.67v22.276h14.72c2.211,0,2.272-1.307,2.272-2.912V39.866h6.019C68.493,39.866,69.048,38.613,67.572,37.08z"></path>
-                </svg>
-                <div className="after" onClick={() => { message.success("You're the best. I didn't think it when I said it, but I said it when I thought about it") }} >
-                  you see what ? 👀
+            [0, 1, 2, 3, 4, 5, 6, 7, 8].map((i,index) => {
 
-                  {/* <Context.Provider value={{ name: 'blog' }}>
-                    {contextHolder}
-                    <div onClick={(e) => openNotification('topRight',e)}>
-                      you see what ? 👀
-                    </div>
-                  </Context.Provider> */}
+               return (
+                <div key={i}>
+                  {ICON[index > 1 ? index%2 :index]}
+                  <div className="after" onClick={() => { message.success("You're the best. I didn't think it when I said it, but I said it when I thought about it") }} >
+                    you see what ? 👀
+                
+                    {/* <Context.Provider value={{ name: 'blog' }}>
+                      {contextHolder}
+                      <div onClick={(e) => openNotification('topRight',e)}>
+                        you see what ? 👀
+                      </div>
+                    </Context.Provider> */}
+                  </div>
                 </div>
-              </div>
-            ))
+              )
+            })
           }
         </div>
       </Modal>
