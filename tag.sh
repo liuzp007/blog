6f21fbfb0bf36d30bf1fb79bca3c34859aff7a61
liuzhipeng <9023287+Roc-js@user.noreply.gitee.com>
2024-05-15 03:39:49 +0000

    add tag.sh.
    Signed-off-by: liuzhipeng <9023287+Roc-js@user.noreply.gitee.com>

---
+++ tag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+prefix="test-"
+
+if `git status | grep "master" &>/dev/null`; then
+    prefix="pro-"
+fi
+
+if [ $1 ] && [ $1 = "pre-" ]; then
+    prefix="pre-"
+fi
+
+function new-tag() {
+    git push
+    git pull --tags
+    local new_tag=$(echo ${prefix}new-$(date +'%Y%m%d')-$(git tag -l "${prefix}new-$(date +'%Y%m%d')-*" | wc -l | xargs printf '%02d'))
+    echo ${new_tag}
+    git tag ${new_tag}
+    git push origin $new_tag
+}
+
+new-tag;
